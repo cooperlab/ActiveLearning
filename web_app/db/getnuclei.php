8e01d828f5bf78051e9cca7183d0a1a9046cd75d
Michael Nalisnik <mnalisnik@emory.edu>
2015-03-06 12:56:13 -0500

    Removed the limit when fetching boundaries from the database.

--- web_app/db/getnuclei.php
+++ web_app/db/getnuclei.php
@@ -89,7 +89,7 @@
 
 
 	$dbConn = boundaryConnect();
-	$sql = 'SELECT boundary, id, centroid_x, centroid_y from boundaries where slide="'.$slide.'" AND centroid_x BETWEEN '.$left.' AND '.$right.' AND centroid_y BETWEEN '.$top.' AND '.$bottom.' LIMIT 15000';
+	$sql = 'SELECT boundary, id, centroid_x, centroid_y from boundaries where slide="'.$slide.'" AND centroid_x BETWEEN '.$left.' AND '.$right.' AND centroid_y BETWEEN '.$top.' AND '.$bottom;
 
 	if( $result = mysqli_query($dbConn, $sql) ) {
 
