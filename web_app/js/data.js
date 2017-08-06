6a513f4000412e6bb91166c3f4686e82841c055e
Sanghoon Lee <shlee8020@gmail.com>
2017-08-06 12:50:35 -0400

    Modified data.js updated data visualization and table

--- web_app/js/data.js
+++ web_app/js/data.js
@@ -25,12 +25,16 @@
 //
 //
 var uid = "";
-var featurename = "";
 var application = "";
+var projectDirMain = "";
+var projectDir ="";
+var array_mean = "";
 
 $(function() {
 
 	application = $_GET('application');
+	projectDirMain = "/fastdata/features/";
+
 	$("#applicationSel").val(application);
 	$("#applicationSeldel").val(application);
 
@@ -41,83 +45,23 @@ $(function() {
 	document.getElementById("nav_data").setAttribute("href","data.html?application="+application);
 	document.getElementById("nav_validation").setAttribute("href","validation.html?application="+application);
 
-	var	boundarySel = $("#boundarySel"),
-		pyramidSel = $("#pyramidSel"), slideSel = $("#slideSel"),
-		slideInfo = $('#slideInfo'), deleteDatasetSel = $("#deleteDatasetSel");
-
-		$('#progDiag').modal('hide');
-		$('#_form').submit(function() {
-    		$('#progDiag').modal('show');
-		});
-
 	$.ajax({
 		type: "POST",
-		url: "php/data_getdatasetsfromdir.php",
-		data: "",
+		url: "php/data_getprojects.php",
+		data: { projectDirMain: projectDirMain },
 		dataType: "json",
 		success: function(data) {
 			// set first featurename
-			for (var i = 0; i < data['dirNames'].length; i++) {
-				$("#datasetSel").append(new Option(data['dirNames'][i], data['dirNames'][i]));
+			for (var i = 0; i < data['projectDir'].length; i++) {
+				$("#projectSel").append(new Option(data['projectDir'][i], data['projectDir'][i]));
 			}
-			//updateFeatures();
+			updateFeature();
 		}
 	});
 
-	$.ajax({
-		type: "POST",
-		url: "php/data_getboundariesfromdir.php",
-		data: "",
-		dataType: "json",
-		success: function(data) {
-
-			for (var i = 0; i < data['dirNames'].length; i++) {
-				boundarySel.append(new Option(data['dirNames'][i], data['dirNames'][i]));
-			}
-			//updateSlideList();
-		}
-	});
-
-	$.ajax({
-		type: "POST",
-		url: "php/data_getpyramidinfofromdir.php",
-		data: "",
-		dataType: "json",
-		success: function(data) {
-
-			for (var i = 0; i < data['dirNames'].length; i++) {
-				pyramidSel.append(new Option(data['dirNames'][i], data['dirNames'][i]));
-			}
-			//updateSlideList();
-		}
-	});
-
-	$.ajax({
-		type: "POST",
-		url: "php/data_getslideinfofromdir.php",
-		data: "",
-		dataType: "json",
-		success: function(data) {
-
-			for (var i = 0; i < data['dirNames'].length; i++) {
-				slideSel.append(new Option(data['dirNames'][i], data['dirNames'][i]));
-			}
-			//updateSlideList();
-		}
-	});
-	// Need to montior changes for the map score select controls. Slide image
-	//	size is dependant on these.
-	//
-	//$("#datasetSel").change(updateDataset);
-	//$("#boundarySel").change(updateBoundaries);
-	$('input[type="radio"]').on('change', function(){
-    if($('#slideInfo').is(':checked')) {
-        $('#pyramidSel').show();
-    } else {
-        $('#pyramidSel').hide();
-    }
-	})
-
+	$("#projectSel").change(updateFeature);
+	$("#featureSel").change(updateFeatureInfo);
+	$("#pyramidSel").change(updatePyramidTable);
 
 	$.ajax({
 		type: "POST",
@@ -127,36 +71,384 @@ $(function() {
 		success: function(data) {
 
 			for( var item in data ) {
-				deleteDatasetSel.append(new Option(data[item][0], data[item][0]));
+				$("#deleteDatasetSel").append(new Option(data[item][0], data[item][0]));
 			}
 		}
 	});
 
-	$("#datasetSel").change(updateFeatures);
+	//$("#datasetSel").change(updateFeatures);
+	$('#progDiag').modal('hide');
+	$('#_form').submit(function() {
+  		$('#progDiag').modal('show');
+	});
+
 
 });
+
+
+
+function updateChart() {
+
+	$('#chart').empty();
+
+	var w = 300,
+	h = 300;
+
+	var colorscale = d3.scale.category10();
+
+	//Legend titles
+	//var LegendOptions = [];
+	//LegendOptions.push(array_slides[0]);
+
+	var d = [];
+	for (var i = 0; i < 1; i++) {
+	 	 d[i] = [];
+	}
+
+	//for (var i = 0; i < array_slides.length; i++) {
+ for (var j = 0; j < array_mean.length; j++) {
+	 d[0].push({
+			 axis: j.toString(),
+			 value: array_mean[j]
+		 });
+ }
+	//}
+
+	//Options for the Radar chart, other than default
+	var mycfg = {
+	 w: w,
+	 h: h,
+	 maxValue: -1,
+	 levels: 6,
+	 ExtraWidthX: 300
+	}
+
+	RadarChart.draw("#chart", d, mycfg);
+}
+
+
+
+function updateTable() {
+
+	$('#pyramidTable').empty();
+
+	var column_names = ["Slide name","Width","Height","Scale"];
+	var clicks = {slides: 0, width: 0, height: 0, scale: 0};
+
+	var table = d3.select("#pyramidTable").append("table");
+	table.append("thead").append("tr");
+
+	var headers = table.select("tr").selectAll("th")
+			.data(column_names)
+		.enter()
+			.append("th")
+			.text(function(d) { return d; });
+
+	var rows, row_entries, row_entries_no_anchor, row_entries_with_anchor;
+
+	d3.json("csv/data.json", function(data) { // loading data from server
+
+		// draw table body with rows
+		table.append("tbody")
+
+		// data bind
+		rows = table.select("tbody").selectAll("tr")
+			.data(data, function(d){ return d.slides; });
+
+		// enter the rows
+		rows.enter()
+			.append("tr")
+
+		// enter td's in each row
+		row_entries = rows.selectAll("td")
+				.data(function(d) {
+					var arr = [];
+					for (var k in d) {
+						if (d.hasOwnProperty(k)) {
+					arr.push(d[k]);
+						}
+					}
+					return [arr[0],arr[1],arr[2],arr[4]];
+				})
+			.enter()
+				.append("td")
+
+		// draw row entries with no anchor
+		row_entries_no_anchor = row_entries.filter(function(d) {
+			return (/https?:\/\//.test(d) == false)
+		})
+		row_entries_no_anchor.text(function(d) { return d; })
+
+		/**  sort functionality **/
+		headers
+			.on("click", function(d) {
+				if (d == "Slide name") {
+					clicks.slides++;
+					// even number of clicks
+					if (clicks.slides % 2 == 0) {
+						// sort ascending: alphabetically
+						rows.sort(function(a,b) {
+							if (a.slides.toUpperCase() < b.slides.toUpperCase()) {
+								return -1;
+							} else if (a.slides.toUpperCase() > b.slides.toUpperCase()) {
+								return 1;
+							} else {
+								return 0;
+							}
+						});
+					// odd number of clicks
+					} else if (clicks.slides % 2 != 0) {
+						// sort descending: alphabetically
+						rows.sort(function(a,b) {
+							if (a.slides.toUpperCase() < b.slides.toUpperCase()) {
+								return 1;
+							} else if (a.slides.toUpperCase() > b.slides.toUpperCase()) {
+								return -1;
+							} else {
+								return 0;
+							}
+						});
+					}
+				}
+				if (d == "Width") {
+				clicks.width++;
+					// even number of clicks
+					if (clicks.width % 2 == 0) {
+						// sort ascending: numerically
+						rows.sort(function(a,b) {
+							if (+a.width < +b.width) {
+								return -1;
+							} else if (+a.width > +b.width) {
+								return 1;
+							} else {
+								return 0;
+							}
+						});
+					// odd number of clicks
+					} else if (clicks.width % 2 != 0) {
+						// sort descending: numerically
+						rows.sort(function(a,b) {
+							if (+a.width < +b.width) {
+								return 1;
+							} else if (+a.width > +b.width) {
+								return -1;
+							} else {
+								return 0;
+							}
+						});
+					}
+				}
+				if (d == "Height") {
+					clicks.height++;
+						// even number of clicks
+						if (clicks.height % 2 == 0) {
+							// sort ascending: numerically
+							rows.sort(function(a,b) {
+								if (+a.height < +b.height) {
+									return -1;
+								} else if (+a.height > +b.height) {
+									return 1;
+								} else {
+									return 0;
+								}
+							});
+						// odd number of clicks
+						} else if (clicks.height % 2 != 0) {
+							// sort descending: numerically
+							rows.sort(function(a,b) {
+								if (+a.height < +b.height) {
+									return 1;
+								} else if (+a.height > +b.height) {
+									return -1;
+								} else {
+									return 0;
+								}
+							});
+						}
+				}
+
+				if (d == "Scale") {
+				clicks.scale++;
+					// even number of clicks
+					if (clicks.scale % 2 == 0) {
+						// sort ascending: numerically
+						rows.sort(function(a,b) {
+							if (+a.scale < +b.scale) {
+								return -1;
+							} else if (+a.scale > +b.scale) {
+								return 1;
+							} else {
+								return 0;
+							}
+						});
+					// odd number of clicks
+					} else if (clicks.scale % 2 != 0) {
+						// sort descending: numerically
+						rows.sort(function(a,b) {
+							if (+a.scale < +b.scale) {
+								return 1;
+							} else if (+a.scale > +b.scale) {
+								return -1;
+							} else {
+								return 0;
+							}
+						});
+					}
+				}
+			}) // end of click listeners
+	});
+	d3.select(self.frameElement).style("height", "300px").style("width", "400px");
+
+}
+
+
+
+function updatePyramidTable() {
+
+	var csvFile = $("#pyramidSel :selected").text();
+
+	$.ajax({
+		type: "POST",
+		url: "php/data_csvtojson.php",
+		data: { csvFile: csvFile },
+		dataType: "json",
+		success: function(data) {
+
+			$('#pyramidTable').empty();
+			var test = data;
+			updateTable();
+		}
+	});
+
+}
+
+
+
+function updatePyramid(){
+
+	projectDir = projectDirMain+projectSel.options[projectSel.selectedIndex].label;
+	var pyramidSel = pyramidSel.options[pyramidSel.selectedIndex].label;
+
+	$.ajax({
+		type: "POST",
+		url: "php/data_getpyramidinfofromdir.php",
+		data: { projectDir: projectDir },
+		dataType: "json",
+		success: function(data) {
+
+			$('#pyramidSel').empty();
+
+			for (var i = 0; i < data['slideInfo'].length; i++) {
+				$("#pyramidSel").append(new Option(data['slideInfo'][i], data['slideInfo'][i]));
+			}
+			if (document.getElementById('pyramidSel').options.length > 0) {
+				updatePyramidTable();
+			}
+
+		}
+	});
+
+}
+//
+//	Updates the list of feature files located in the project directory
+//
+function updateFeature() {
+
+	projectDir = projectDirMain+projectSel.options[projectSel.selectedIndex].label;
+
+	$('#pyramidSel').empty();
+	$('#featureSel').empty();
+	$('#boundarySel').empty();
+	$('#chart').empty();
+	$('#pyramidTable').empty();
+
+
+	$.ajax({
+		type: "POST",
+		url: "php/data_getdatasetsfromdir.php",
+		data: { projectDir: projectDir,
+						application: application },
+		dataType: "json",
+		success: function(data) {
+
+			for (var i = 0; i < data['featureName'].length; i++) {
+				$("#featureSel").append(new Option(data['featureName'][i], data['featureName'][i]));
+			}
+			if (document.getElementById('featureSel').options.length > 0) {
+				updateFeatureInfo();
+			}
+		}
+	});
+
+	$.ajax({
+		type: "POST",
+		url: "php/data_getboundariesfromdir.php",
+		data: { projectDir: projectDir },
+		dataType: "json",
+		success: function(data) {
+
+			for (var i = 0; i < data['boundaryDir'].length; i++) {
+				$("#boundarySel").append(new Option(data['boundaryDir'][i], data['boundaryDir'][i]));
+			}
+			//updateFeatures();
+		}
+	});
+
+	$.ajax({
+		type: "POST",
+		url: "php/data_getpyramidinfofromdir.php",
+		data: { projectDir: projectDir },
+		dataType: "json",
+		success: function(data) {
+
+			for (var i = 0; i < data['slideInfo'].length; i++) {
+				$("#pyramidSel").append(new Option(data['slideInfo'][i], data['slideInfo'][i]));
+			}
+			if (document.getElementById('pyramidSel').options.length > 0) {
+				updatePyramidTable();
+			}
+
+		}
+	});
+
+}
 
 
 //
 //	Updates the list of available slides for the current dataset
 //
-function updateFeatures() {
+function updateFeatureInfo() {
 
-	featurename = datasetSel.options[datasetSel.selectedIndex].label;
-	//document.getElementById('featureName').innerHTML = featurename;
+	var featurename = featureSel.options[featureSel.selectedIndex].label;
+	var delay = 2000;
+
+	$('#progDiag').modal('show');
 
 	// Get the information for the current dataset
-	// $.ajax({
-	// 	type: "POST",
-	// 	url: "php/data_getfeatures.php",
-	// 	data: { featurename: featurename },
-	// 	dataType: "json",
-	// 	success: function(data) {
-	//
-  //     document.getElementById('featureInfo').innerHTML = data['features'];
-	//
-	// 	}
-	// });
+	 $.ajax({
+	 	type: "POST",
+	 	url: "php/data_getfeatures.php",
+		data: { projectDir: projectDir,
+						featurename: featurename },
+	 	dataType: "json",
+	 	success: function(data) {
+			setTimeout(function() {
+			 delaySuccess(data);
+			  }, delay);
+	 	}
+	 });
+
+
+}
+
+function delaySuccess(data) {
+
+	array_mean = data['mean'];
+
+	updateChart();
+
+	$('#progDiag').modal('hide');
+
 }
 
 //
