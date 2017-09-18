a638c267a94e8ff504def908e776c1a58a549d3a
slee172 <shlee8020@gmail.com>
2017-09-18 16:59:00 -0400

    Modified tableContent.php - survival

--- web_app/python/tableContent.php
+++ web_app/python/tableContent.php
@@ -1,28 +1,21 @@
-
-<!DOCTYPE html>
-<html lang="en">
-	 <head>
-				<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-				<meta content="utf-8" http-equiv="encoding">
-      </head>
-    <body>
-
 <?php
-		// require '../db/logging.php';
 		if(isset($_POST['uni_coeff'])) {
 			$csv_data = $_POST['uni_coeff'];
 			file_put_contents("uni_coeff.json", $csv_data);
 			$uni_result = shell_exec('python multiCoeff.py uni_coeff.json uni');
-			$resultData = json_decode($uni_result, true);
-			var_dump($resultData);
-
 		}else if(isset($_POST['multi_coeff'])){
 			$json = $_POST['multi_coeff'];
 			file_put_contents("multi_coeff.json", $json);
 			$multi_result = shell_exec('python multiCoeff.py multi_coeff.json multi');
-			$resultData = json_decode($multi_result, true);
-			var_dump($resultData);
+			$result = json_decode($multi_result);
+			$test = array("multi" => $result);
+      echo json_encode($test);
+		}else if(isset($_POST['logrank'])){
+			$log_data = $_POST['logrank'];
+			file_put_contents("logrank.json", $log_data);
+			$logrank_result = shell_exec('python getlogrank.py logrank.json');
+			$rst = json_decode($logrank_result);
+			$dt = array("lg" => $rst);
+			echo json_encode($dt);
 		}
 ?>
-    </body>
-</html>
